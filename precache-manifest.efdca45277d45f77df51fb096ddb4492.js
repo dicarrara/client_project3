99b31fc10315e089228b861460902101d145a37b
Diana Carrara <dianaVcarrara@gmail.com>
2019-08-03 10:56:05 -0600

    Updates

---
+++ precache-manifest.efdca45277d45f77df51fb096ddb4492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16088d20412231d219e80d44e89f2f20",
+    "url": "/dicarrara/client_project3/index.html"
+  },
+  {
+    "revision": "44dd36bc45cc332e7008",
+    "url": "/dicarrara/client_project3/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/dicarrara/client_project3/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "44dd36bc45cc332e7008",
+    "url": "/dicarrara/client_project3/static/js/main.ca913320.chunk.js"
+  },
+  {
+    "revision": "1801289032511fcc03f1",
+    "url": "/dicarrara/client_project3/static/js/runtime~main.1b94b1af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dicarrara/client_project3/static/media/logo.5d5d9eef.svg"
+  }
+]);
