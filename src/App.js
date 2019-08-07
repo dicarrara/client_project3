c62066d4be4532553721bfa3d715d97964039680
Diana Carrara <dianaVcarrara@gmail.com>
2019-08-06 20:02:12 -0600

    add app switch to render dif pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
+import LoginScreen from './LoginScreen'
 import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <>
     <Router>
-      <NavBar/>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={LoginScreen} />
+          
+        </Switch>
+      </div>
     </Router>
-    
-    </>
   );
 }
+  
 
 export default App;
+
